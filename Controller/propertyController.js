9587b38ccebedd8d30172f3daafd033e6f71db14
Lapidis2 <hitlapidis2@gmail.com>
2025-03-29 12:38:47 +0200

    new changes added

--- Controller/propertyController.js
+++ Controller/propertyController.js
@@ -66,7 +66,7 @@ const createProperty = async (req, res) => {
             return transporter.sendMail({
               from: process.env.ADMIN_EMAIL,
               to: emailData.email,
-              subject: "New Challenge Added",
+              subject: "New property Added",
               html: `
                 <div style="font-family: Arial, sans-serif; margin: 0; padding: 10px; background-color: #f4f4f4;">
                   <div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
@@ -74,9 +74,9 @@ const createProperty = async (req, res) => {
                     <img src="${newProperty.imageUrl}" alt="Blog Image" style="width: 100%; max-width: 400px; height: auto; margin-bottom: 20px; border-radius: 5px;">
                     <h2 style="font-size: 24px; margin-bottom: 10px;">${newProperty.title}</h2>
                     <p style="color: #666;">Hello there!</p>
-                    <p style="color: #666;">We're excited to inform you that a new challenge post has been added to our website.</p>
+                    <p style="color: #666;">We're excited to inform you that a new property has been added to our website.</p>
                     <p style="color: #666;">Check it out now:</p>
-                    <a href="https://umurava-skill-challenge.netlify.app//openedblog?id=${newProperty._id}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">See house here</a>
+                    <a href="https://paccy-easy-renting-fn.netlify.app/openedblog?id=${newProperty._id}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">See house here</a>
                     <p style="color: #666;">If you have any questions or feedback, feel free to reply to this email.</p>
                     <p style="color: #666;">Thank you for being a valued subscriber!</p>
                     <p style="color: #666;">Best Regards,<br>GREAT CONNECTION GROUP</p>
