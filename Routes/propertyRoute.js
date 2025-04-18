9e2f76a8a5d6374476ee3cebe1629f405d5acfdb
habyarimana caleb <callebhabyar55@gmail.com>
2025-04-18 18:56:59 +0200

    update assets routes

--- Routes/propertyRoute.js
+++ Routes/propertyRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../Config/multer");
-const {createProperty,getProperties,deleteProperty,getPropertyById,updateProperty} = require("../Controller/propertyController");
+const {createProperty,getProperties,deleteProperty,getPropertyById,updateProperty,getPropertiesByType} = require("../Controller/propertyController");
 
 router.post("/create-property",
   upload.single("image"),
   createProperty
 );
-router.get("/get-property/:id",getPropertyById);
-router.delete("/delete-property/:id",deleteProperty);
+router.get("/get-properties/type/:type", getPropertiesByType);
+router.get("/get-property/:id" , getPropertyById);
+router.delete("/delete-property/:id" , deleteProperty);
 router.put("/update-property/:id",
   upload.single("image"),
   updateProperty
