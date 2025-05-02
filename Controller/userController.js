ff642ec654fddab993e518e930b45e40fe9da41b
Lapidis2 <hitlapidis2@gmail.com>
2025-05-02 21:36:42 +0200

    Updating user information

--- Controller/userController.js
+++ Controller/userController.js
@@ -3,18 +3,19 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE === "true", // Use SSL
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false,
+	auth: {
+	  user: process.env.ADMIN_EMAIL,
+	  pass: process.env.ADMIN_PSWD,
+	},
+	tls: {
+	  rejectUnauthorized: false,
+	},
 });
 
 // Query to check if a user exists by email
