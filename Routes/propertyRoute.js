6d41a6ff1d17e9b131d29438fdce36344a204a7b
habyarimana caleb <callebhabyar55@gmail.com>
2025-05-12 17:27:12 +0200

    add dashboard connectivity and handle bugs

--- Routes/propertyRoute.js
+++ Routes/propertyRoute.js
@@ -3,17 +3,12 @@ const router = express.Router();
 const upload = require("../Config/multer");
 const {createProperty,getProperties,deleteProperty,getPropertyById,updateProperty,getPropertiesByType} = require("../Controller/propertyController");
 
-router.post("/create-property",
-  upload.single("image"),
-  createProperty
+router.post("/create-property",upload.single("image"),createProperty
 );
 router.get("/get-properties/type/:type", getPropertiesByType);
 router.get("/get-property/:id" , getPropertyById);
 router.delete("/delete-property/:id" , deleteProperty);
-router.put("/update-property/:id",
-  upload.single("image"),
-  updateProperty
-);
+router.put("/update-property/:id",upload.single("image"),updateProperty);
 router.get("/get-properties",getProperties);
 
 module.exports = router;
